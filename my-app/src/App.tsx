70b7873e31e5b263acc46ca5ec7bcc747018e873
HeegwonYang <122384875+HeegwonYang@users.noreply.github.com>
2025-09-20 16:52:02 -0400

    React routers

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,37 +1,92 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import msgLogo from './assets/chat-bubble-user-svgrepo-com.svg'
+import phishLogo from './assets/fishing-hook-3-svgrepo-com.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Start(){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Who's the Scammer?</h1>
-      <div className="card">
+      <h1>Scam Spotter</h1>
+      <div className="intro">
         <p>
-          "Who's the Scammer?" is a game where you will be presented with five different
-          <br />fictional examples of common scams in real life. To each of these examples,
-          <br />you will be asked to swipe LEFT if you think it's a scam, or RIGHT if it seems trustworthy.
+          Scam Spotter is a mobile learning game designed to help adults practice identifying scams in a safe,
+          interactive environment. With real-world inspired, AI-generated examples, users sharpen their ability
+          to recognize fraudulent messages and build confidence in protecting themselves online.
           <br /><br />
-          Use your best judgement and try to guess correctly for all five!
+          To start, choose a unit you want to play through from the options at the bottom. Then, for each message that
+          appears, swipe LEFT if you think the message is a scam, or RIGHT if you think the message is trustworthy.
+          If you're stuck, press the Hint button to show common red flags that might be present in the message.
+          <br /><br />
+          Each unit has 5 questions. Get all 5 correct, and you'll earn a badge for that unit!
         </p> 
-        <button onClick={() => setCount((count) => count + 1)}>
-          Let's start!
-        </button>
+        <Link to="/home"><button>Let's start!</button></Link>
         <p>
         </p>
       </div>
     </>
+  )
+}
+
+function Home(){
+  return(
+
+    // if the flag for a pe
+    <div>
+      <Link to="/messages">
+        <div>
+        <h3>
+            <img src={msgLogo} className="msg logo" alt="scam messages logo" />
+        Scam Messages</h3>
+        </div>
+      </Link>
+      <Link to="/phishing">
+      <div>
+        <h3>
+          <img src={phishLogo} className="phish logo" alt="Phishing logo" />
+        Phishing
+        </h3>
+      </div>
+          
+      </Link>
+      <Link to="/social"><button>Social Media Scams</button></Link>
+      <Link to="/misinfo"><button>Fake News & Misinformation </button></Link>
+    </div>
+  )
+}
+
+function Messages(){
+  return <h1>placeholder for messages</h1>
+}
+
+function Phishing(){
+  return <h1>placeholder for phishing</h1>
+}
+
+function Social(){
+  return <h1>placeholder for social</h1>
+}
+function Misinfo(){
+  return <h1>placeholder for misinfo</h1>
+}
+
+
+function App() {
+  const [introCard, displayIntro] = useState(true)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/phishing" element={<Phishing />} />
+        <Route path="/social" element={<Social />} />
+        <Route path="/misinfo" element={<Misinfo />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
