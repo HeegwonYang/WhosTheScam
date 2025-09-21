516f0e4535bfb8a86e27f21657a47fa00128ee02
a2un <encodedgeek@gmail.com>
2025-09-21 10:21:11 -0400

    add update score

--- scam_add_generator_api/generate_ads.py
+++ scam_add_generator_api/generate_ads.py
@@ -1,9 +1,12 @@
 import os
-from os.path import join
+from os.path import join,exists
 from pydoc import text
 from google.genai import Client
 from utils import get_model_response
 from fastapi import FastAPI
+import json
+import random
+from datetime import datetime
 
 app = FastAPI()
 
@@ -19,7 +22,36 @@ def generate_ads(level=1,sublevel=1,num_messages=10):
     # print(text_messages)
     return text_messages
 
+@app.get("/update_score/{student_id}/{level}/{sublevel}/{score}")
+def update_score(student_id=1,level=1,sublevel=1,score=1):
+    
+    try:
+        if not(exists('./student_scores')): os.mkdir('./student_scores')
+        timestamp = datetime.now().strftime(f'%Y-%m-%d-%H-%M-%S-{random.randint(10**2, 10**3-1)}')
 
+        with open(f'./student_scores/{student_id}_{level}_{sublevel}_{timestamp}.json','w+') as f: 
+            json.dump({"score":score},f)
+
+        return "success"
+    except e:
+        print(e)
+        return "failed"
+
+
+@app.get("/update_correctness/{student_id}/{level}/{sublevel}/{question}/{correct_answer}/{student_answer}")
+def update_correctness(student_id=1,level=1,sublevel=1,score=1):
+    
+    try:
+        if not(exists('./student_answers')): os.mkdir('./student_answers')
+        timestamp = datetime.now().strftime(f'%Y-%m-%d-%H-%M-%S-{random.randint(10**2, 10**3-1)}') 
+
+        with open(f'./student_scores/{student_id}_{level}_{sublevel}_{timestamp}.json','w+') as f: 
+            json.dump({"question":question,"correct_answer":correct_answer, "student_answer":student_answer},f)
+
+        return "success"
+    except e:
+        print(e)
+        return "failed"
 
 if __name__ == "__main__":
     generate_ads()
